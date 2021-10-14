81f6b0ce75a49465ac70eb0cca039c014c3634ea
Victor Ramos <victor@aiconsult.business>
2021-10-14 19:46:47 -0300

    atualizaçoes

--- script.py
+++ script.py
@@ -2,28 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-img = cv2.imread('img.jpg')
+img = cv2.imread("img.jpg")
+
 
 def view(img):
     plt.imshow(img)
     plt.show()
-    plt.hist(img.ravel(), 256, [0,256])
+    plt.hist(img.ravel(), 256, [0, 256])
     plt.show()
-    
+
 
 def log_transformation(img):
-    num = float(input('Digite o valor a se calculado o log(>0):'))
+    num = float(input("Digite o valor a se calculado o log(>0):"))
     c = 255 / (np.log(num))
-    log_img = np.array(c * (np.log(img + 1)), dtype = np.uint8)
+    log_img = np.array(c * (np.log(img + 1)), dtype=np.uint8)
     view(log_img)
+
 
 def inv_transformation(img):
     inv_img = 255 - img
     view(inv_img)
 
+
 def expo_transformation(img):
-    expo = float(input('Digite o valor do expoente:'))
-    expo_img = np.array(255*(img/255)**expo, dtype = 'uint8')
+    expo = float(input("Digite o valor do expoente:"))
+    expo_img = np.array(255 * (img / 255) ** expo, dtype="uint8")
     view(expo_img)
 
 
